d876a9e82fcb791595d5f1d73f63e9e131ff7c8c
seongit <seongeunk102@gmail.com>
2023-03-06 17:29:35 +0900

    feat : websocket 기능 끄기
    1. 서버에 배포 후 ws connection to failed에러가 밠생하여 다음과 같이 변경합니다.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,18 +8,24 @@ module.exports = defineConfig({
   // npm run serve 개발 진행시에 포트가 다르기때문에 프록시 설정
    devServer: {
      //proxy: 'http://localhost:8081' // api 서버 포트 번호로 지정
-    proxy : process.env.VUE_API_URL || 'http://localhost:8081' 
+     proxy: {
+      '/':{
+          target : process.env.VUE_API_URL, // 레드마인 api 주소 및 포트
+          changeOrigin:true,  
+          ws:false
+      }
+  }
   },
 
   // nginx reverse proxy 설정 시 다음과 같이 구성함
 //   devServer: {
-//       proxy: {
-//           '/':{
-//               "target":'http://100.0.0.157:30180', // 레드마인 api 주소 및 포트
-//               "pathRewrite":{'^/':''},
-//               "changeOrigin":true,  
-//           }
-//       }
+      // proxy: {
+      //     '/':{
+      //         "target":'http://100.0.0.157:30180', // 레드마인 api 주소 및 포트
+      //         "pathRewrite":{'^/':''},
+      //         "changeOrigin":true,  
+      //     }
+      // }
 //     }
 
 });
